db724508d4b4483035e16c64df5305c2d45439f5
Rashed Suwaidi <x.suwaidi@gmail.com>
2023-11-29 14:24:31 +0400

    finish rules

--- openfisca_dubai/variables/taxes.py
+++ openfisca_dubai/variables/taxes.py
@@ -64,6 +64,44 @@ class corporate_tax(Variable):
         return tax_payable * is_exempt
 
 
+class taxable_income(Variable):
+    value_type = float
+    entity = entities.Person
+    definition_period = periods.YEAR
+    label = (
+        "The income that is subject to Corporate Tax under Federal Decree Law No. 47"
+    )
+    reference = "https://mof.gov.ae/wp-content/uploads/2022/12/Federal-Decree-Law-No.-47-of-2022-EN.pdf"
+
+    # Can add a formula later
+    def formula(person, period, parameters):
+        tax_credits = person("tax_credits", period)
+        interest_expense = person("interest_expense", period)
+        interest_income = person("interest_income", period)
+        depreciation = person("depreciation", period)
+        ebitda = person("EBITDA", period)
+        amortization = person("amortization", period)
+        carry_forward_interest = person("carry_forward_interest", period)
+
+        net_interest = interest_expense - interest_income
+        max_interest_deduction = max(0.3 * ebitda, 12000000)
+        net_interest = min(net_interest, max_interest_deduction)
+        carry_forward_interest = min(
+            carry_forward_interest, 0.3 * ebitda - net_interest
+        )
+        net_interest += carry_forward_interest
+
+        taxable_income = ebitda - net_interest
+        taxable_income -= depreciation
+        taxable_income -= amortization
+
+        max_tax_credits = 0.75 * taxable_income
+        actual_tax_credits = min(tax_credits, max_tax_credits)
+        taxable_income -= actual_tax_credits
+
+        return taxable_income
+
+
 class tax_credits(Variable):
     value_type = float
     entity = entities.Person
@@ -85,68 +123,6 @@ class is_pension_fund(Variable):
     entity = entities.Person
     definition_period = periods.YEAR
     label = "A pension fund"
-    reference = "https://mof.gov.ae/wp-content/uploads/2022/12/Federal-Decree-Law-No.-47-of-2022-EN.pdf"
-
-
-class taxable_income(Variable):
-    value_type = float
-    entity = entities.Person
-    definition_period = periods.YEAR
-    label = (
-        "The income that is subject to Corporate Tax under Federal Decree Law No. 47"
-    )
-    reference = "https://mof.gov.ae/wp-content/uploads/2022/12/Federal-Decree-Law-No.-47-of-2022-EN.pdf"
-
-    # Can add a formula later
-    def formula(person, period, parameters):
-        interest_expense = person("interest_expense", period)
-        interest_income = person("interest_income", period)
-        depreciation = person("depreciation", period)
-        ebitda = person("EBITDA", period)
-        amortization = person("amortization", period)
-
-        net_interest = interest_expense - interest_income
-        max_interest_deduction = max(0.3 * ebitda, 12000000)
-        accounting_income = ebitda
-
-        print(f"net_interest: {net_interest}")
-        print(f"max_interest_deduction: {max_interest_deduction}")
-        print(f"accounting_income: {accounting_income}")
-
-        if net_interest > max_interest_deduction:
-            disallowed_interest = net_interest - max_interest_deduction
-            accounting_income -= disallowed_interest
-        else:
-            accounting_income -= max_interest_deduction
-
-        accounting_income -= depreciation
-        accounting_income -= amortization
-
-        print(f"accounting_income: {accounting_income}")
-
-        taxable_income = accounting_income
-
-        try:
-            tax_credits = person("tax_credits", period)
-        except:
-            tax_credits = 0
-
-        max_tax_credits = 0.75 * taxable_income
-        actual_tax_credits = min(tax_credits, max_tax_credits)
-        taxable_income -= actual_tax_credits
-
-        return taxable_income
-
-
-class accounting_income(Variable):
-    value_type = float
-    entity = entities.Person
-    definition_period = periods.YEAR
-    label = """
-    The accounting net profit or loss for the relevant Tax Period
-    as per the financial statements prepared in accordance
-    with the provisions of Article 20 of this Decree-Law
-    """
     reference = "https://mof.gov.ae/wp-content/uploads/2022/12/Federal-Decree-Law-No.-47-of-2022-EN.pdf"
 
 
@@ -196,6 +172,17 @@ class amortization(Variable):
     definition_period = periods.YEAR
     label = """
     Interest Expense
+    """
+    reference = "https://mof.gov.ae/wp-content/uploads/2022/12/Federal-Decree-Law-No.-47-of-2022-EN.pdf"
+
+
+class carry_forward_interest(Variable):
+    value_type = float
+    entity = entities.Person
+    definition_period = periods.YEAR
+    label = """
+    A Taxable Person can carry forward interest expense and offset them against Taxable
+    Income in subsequent Tax Periods
     """
     reference = "https://mof.gov.ae/wp-content/uploads/2022/12/Federal-Decree-Law-No.-47-of-2022-EN.pdf"
 
